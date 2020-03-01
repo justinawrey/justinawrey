6d0da27902024a3805976500d518931b4530552f
Justin Awrey <awreyjustin@gmail.com>
2020-02-29 23:09:50 -0800

    more fixing

--- functions/email.js
+++ functions/email.js
@@ -3,8 +3,6 @@ const sgMail = require("@sendgrid/mail");
 exports.handler = async event => {
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
-  console.warn(process.env.SENDGRID_API_KEY);
-
   // Only allow POST
   if (event.httpMethod !== "POST") {
     return { statusCode: 405, body: "Method Not Allowed" };
@@ -12,38 +10,31 @@ exports.handler = async event => {
 
   const { name, email, subject, message } = JSON.parse(event.body);
 
+  console.log("NEW MESSAGE");
+  console.log("name", name);
+  console.log("email", email);
+  console.log("subject", subject);
+  console.log("message", message);
+
   // goes to me
-  sgMail
-    .send({
-      to: "awreyjustin@gmail.com",
-      from: "noreply@justinawrey.com",
-      subject: `[${name} | ${email}] ${subject}`,
-      text: message
-    })
-    .then(() => ({
-      statusCode: 200,
-      body: "success"
-    }))
-    .catch(() => ({
-      statusCode: 500,
-      body: "failure"
-    }));
+  const first = sgMail.send({
+    to: "awreyjustin@gmail.com",
+    from: "noreply@justinawrey.com",
+    subject: `[${name} | ${email}] ${subject}`,
+    text: message
+  });
 
   // goes to sender
-  sgMail
-    .send({
-      to: email,
-      from: "noreply@justinawrey.com",
-      subject: "Thanks for the inquiry!",
-      text:
-        "I've received your message and will get back to you as soon as possible.  Please do not respond to this email."
-    })
-    .then(() => ({
-      statusCode: 200,
-      body: "success"
-    }))
-    .catch(() => ({
-      statusCode: 500,
-      body: "failure"
-    }));
+  const second = sgMail.send({
+    to: email,
+    from: "noreply@justinawrey.com",
+    subject: "Thanks for the inquiry!",
+    text:
+      "I've received your message and will get back to you as soon as possible.  Please do not respond to this email."
+  });
+
+  return Promise.all([first, second]).then({
+    statusCode: 200,
+    body: "success"
+  });
 };
